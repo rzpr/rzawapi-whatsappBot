0c3001ac238b017e4887a9f617e4aea104aa3ce3
Rezza Priatna <58212770+rezzaapr@users.noreply.github.com>
2022-01-29 19:20:31 +0700

    Add files via upload

---
+++ wa.py
@@ -0,0 +1,53 @@
+import requests as r
+import json
+
+class wa:
+     def __init__(self, json):
+         self.nomor = json['response']['from']
+         self.pesan = json['response']['body']
+         self.apikey = 'botveriv'
+
+     def kirim_requests(self, method,data):
+         url = f"https://rzawapi.my.id/api/{method}"
+         headers = {
+             "apikey": f"{self.apikey}",
+             "Content-Type": "application/json"
+         }
+         kirim = r.post(url, data=json.dumps(data), headers=headers)
+         return kirim.json()
+
+     def sendMessage(self):
+         data = {
+         "to": self.nomor,
+         "body": "pong"
+         }
+         kirim = self.kirim_requests('send-message',data)
+         return kirim
+
+     def sendImage(self):
+         data = {
+         "to": self.nomor,
+         "body": "https://loremflickr.com/320/240",
+         "caption":"image caption"#Kosongkan Jika Tidak Ingin Pakai Caotion
+         }
+         kirim = self.kirim_requests('send-image',data)
+         return kirim
+     def sendLocation(self):
+         data = {
+         "to": self.nomor,
+         "body":"Rumah Saya",
+         "longitude":"106.631889",
+         "latitude":"-6.178306"
+         }
+         kirim = self.kirim_requests('send-location',data)
+         return kirim
+
+     def processing(self):
+         if self.pesan == 'ping':
+             return self.sendMessage()
+         elif self.pesan == 'image':
+             return self.sendImage()
+         elif self.pesan == 'location':
+             return self.sendLocation()
+         else:
+             return self.NoCommand()
